c33347578b00e08e578e423c5677a326ef3a1aad
giovanafarias <giovanafarias731@gmail.com>
2026-09-09 21:36:14 -0300

    configurando as permissoes

--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -14,11 +14,18 @@ class PermissionSeeder extends Seeder
     public function run(): void
     {
         $data = [
-       // PROFESSOR - DISCIPLINA
+            // PROFESSOR - DISCIPLINA
             ["role_id" => 1, "resource_id" => 6],
             ["role_id" => 1, "resource_id" => 7],
             ["role_id" => 1, "resource_id" => 8],
             ["role_id" => 1, "resource_id" => 9],
+            // PROFESSOR - ALUNO
+            ["role_id" => 1, "resource_id" => 11],
+            ["role_id" => 1, "resource_id" => 12],
+            ["role_id" => 1, "resource_id" => 13],
+            ["role_id" => 1, "resource_id" => 14],
+            // PROFESSOR - MATRICULA
+            ["role_id" => 1, "resource_id" => 17],
             //  COORDENADOR - CURSO
             ["role_id" => 2, "resource_id" => 1],
             ["role_id" => 2, "resource_id" => 2],
@@ -31,6 +38,12 @@ class PermissionSeeder extends Seeder
             ["role_id" => 2, "resource_id" => 8],
             ["role_id" => 2, "resource_id" => 9],
             ["role_id" => 2, "resource_id" => 10],
+            // COORDENADOR - ALUNO
+            ["role_id" => 2, "resource_id" => 11],
+            ["role_id" => 2, "resource_id" => 13],
+            // COORDENADOR - MATRICULA
+            ["role_id" => 2, "resource_id" => 17],
+            ["role_id" => 2, "resource_id" => 20],
         ];
         DB::table('permissions')->insert($data);
     }
